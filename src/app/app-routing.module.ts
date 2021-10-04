c5792d5583758ba1656c854151b85b0abf3be3c6
Kira Roberts <kirajroberts@gmail.com>
2021-10-04 11:44:58 -0700

    route profile button on navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashBoardComponent } from './dash-board/dash-board.component';
+import { ProfileComponent } from './profile/profile.component';
 import { ProspectFormsComponent } from './prospect-forms/prospect-forms.component';
 
 const routes: Routes = [
   {path: 'admin', component: DashBoardComponent},
-  {path: 'forms', component: ProspectFormsComponent}
+  {path: 'forms', component: ProspectFormsComponent},
+  {path: 'profile', component: ProfileComponent}
 ];
 
 @NgModule({
